7aa5f907f957125c1b55c28b93022dedc446854e
Erica Moreira <erica.emoreira16@gmail.com>
2024-10-04 16:12:27 -0300

    projeto inicial

---
+++ js-curso-2-aula_-/exe.js
@@ -0,0 +1,18 @@
+let listaGenerica = [];
+let linguagemDeProgramacao = ['JavaScript','C','C++','Kotlin','Python'];
+let listaNomes = [];
+
+
+linguagemDeProgramacao.push('Java','Ruby','GoLang');
+
+listaNomes.push('Levi','Heitor','Rodrigo');
+console.log(listaNomes[0]);
+
+listaNomes.push('Banana','Maca','Abacaxi');
+console.log(listaNomes[1]);
+
+listaNomes.push('Ovo','Macarrao','Molho');
+console.log(listaNomes[2]);
+
+
+
